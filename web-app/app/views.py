3232ad075f87e9f4622cce7eb01c924a9d1e30e5
dorislee0309 <dorislee@berkeley.edu>
2016-10-18 09:36:32 -0500

    displaying objects identified in previous module in the segmentation module

--- web-app/app/views.py
+++ web-app/app/views.py
@@ -20,13 +20,34 @@ def segment(img):
 					"hit_id": request.args.get("hitId")}
 
 	filename = 'static/' + img + '.png'
+
+	#Read objects that have already been identified from the database
+	worker = models.Worker.query.filter_by(turker=request.args.get("workerId")).first()
+	if worker is None and render_data["worker_id"] != 'None':
+		db.session.add(models.Worker(turker=request.args.get("workerId")))
+		db.session.commit()
+	image = models.Image.query.filter_by(filename=img).first()
+	if image is None:
+		db.session.add(models.Image(filename=img))
+		db.session.commit()
+
+	image_id = models.Image.query.filter_by(filename=img).first().id
+
+	objects = models.Object.query.filter_by(image_id=image_id).order_by(models.Object.name).all()
+	#Read object locations for these objects
+	object_locations = models.ObjectLocation.query.filter((models.ObjectLocation.object_id.in_([x.id for x in objects]))).all()
+
+	#Make this data easy to use
+	objects = {x.id:x.name for x in objects}
+	object_locations = {x.object_id:(x.x_loc,x.y_loc) for x in object_locations} #ASSUMES THAT EACH OBJECT IS MARKED BY EXACTLY 1 WORKER
+
 	#The following code segment can be used to check if the turker has accepted the task yet
 	if request.args.get("assignmentId") == "ASSIGNMENT_ID_NOT_AVAILABLE":
 		#Our worker hasn't accepted the HIT (task) yet
-		resp = make_response(render_template('page.html',name=render_data,filename=filename,ht=384,wd=512,accepted=False))
+		resp = make_response(render_template('page.html',name=render_data,filename=filename,ht=384,wd=512,accepted=False,objects=objects,locs=object_locations,img=img))
 	else:
 		#Our worker accepted the task
-		resp = make_response(render_template('page.html',name=render_data,filename=filename,ht=384,wd=512,accepted=True))
+		resp = make_response(render_template('page.html',name=render_data,filename=filename,ht=384,wd=512,accepted=True,objects=objects,locs=object_locations,img=img))
 	resp.headers['x-frame-options'] = 'this_can_be_anything'
 	return resp
 
@@ -149,19 +170,21 @@ def segmentation_submit():
 
 	x_locs = json.loads(request.form['x-locs'])
 	y_locs = json.loads(request.form['y-locs'])
-	# img = json.loads(request.form['image-id'])
+	img = json.loads(request.form['image-id'])
 	# object_names = json.loads(request.form['obj-names'])
 	comment = json.loads(request.form['comment-input'])
 	# #Store all the collected data in the database
 	# assume that the worker and object id is given for each task 
 	worker_id =123
+	print "workerid",request.args.get("workerId")
 	# worker_id = models.Worker.query.filter_by(turker=request.args.get("workerId")).first().id
 	# print "Worker id", worker_id
 	# image_id = models.Image.query.filter_by(filename=img).first().id
 	# print worker_id
 	# print image_id
-	# print "xlocs type:", type(x_locs)
-	img = 54
+	# print "xlocs type:", type(x_locs) 
+	# print "image : ", img
+	# print "image id : ", image_id
 	obj_id = 5
 	bounding_box= models.BoundingBox(object_id=obj_id,worker_id=worker_id,x_locs=str(x_locs),y_locs=str(y_locs))
 	db.session.add(bounding_box)

99549df2a7fd7431f4754e85b4015c122b015624
dorislee0309 <dorislee@berkeley.edu>
2016-10-30 12:40:36 -0500

    added hit info

--- web-app/run.py
+++ web-app/run.py
@@ -3,3 +3,4 @@ from app import app
 import os
 port = int(os.environ.get("PORT", 5000))
 app.run(host='0.0.0.0', port=port, debug=True)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']

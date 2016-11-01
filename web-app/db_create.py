fa7ee9d3b60d031caa820957c8ffa4d76f3ed7a0
dorislee0309 <dorislee@berkeley.edu>
2016-11-01 18:17:35 -0500

    trying to make db work

--- web-app/db_create.py
+++ web-app/db_create.py
@@ -1,13 +1,11 @@
 #!flask/bin/python
 from migrate.versioning import api
-from config import SQLALCHEMY_MIGRATE_REPO,LOCAL
+from config import SQLALCHEMY_DATABASE_URI,SQLALCHEMY_MIGRATE_REPO,LOCAL
 from app import db
 import os.path
 db.create_all()
-if LOCAL:
-	from config import SQLALCHEMY_DATABASE_URI
-	if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
-	    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
-	    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-	else:
-	    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+else:
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))

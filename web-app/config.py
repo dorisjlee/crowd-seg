c8c3ca9a684da35488c198a88d5ae282c5fa4337
dorislee0309 <dorislee@berkeley.edu>
2016-11-01 18:17:35 -0500

    trying to make db work

--- web-app/config.py
+++ web-app/config.py
@@ -1,10 +1,9 @@
-LOCAL=False
+CSRF_ENABLED = True
 WTF_CSRF_ENABLED = True
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-if LOCAL : SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/segment'
-
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/segment'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 

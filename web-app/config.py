9f7ac3d7fe9341fc6a6b0e41dccce3ff234bc772
dorislee0309 <dorislee@berkeley.edu>
2016-11-02 09:53:41 -0500

    chnage db url

--- web-app/config.py
+++ web-app/config.py
@@ -4,6 +4,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/segment'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/segment'
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","postgresql://localhost/segment")
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 

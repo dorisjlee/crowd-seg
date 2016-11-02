71d9d41e00e60ff02094ee0024787acdc0d1a7b7
dorislee0309 <dorislee@berkeley.edu>
2016-11-02 09:21:38 -0500

    postgres  working now

--- web-app/db_create.py
+++ web-app/db_create.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 from migrate.versioning import api
-from config import SQLALCHEMY_DATABASE_URI,SQLALCHEMY_MIGRATE_REPO,LOCAL
+from config import SQLALCHEMY_DATABASE_URI,SQLALCHEMY_MIGRATE_REPO
 from app import db
 import os.path
 db.create_all()
--- web-app/run.py
+++ web-app/run.py
@@ -1,4 +1,3 @@
-#!flask/bin/python
 from app import app
 app.run(debug=True)
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']

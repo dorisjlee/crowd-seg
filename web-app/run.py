16c984f963156a6e02dcf33fc445c3c184d3c7bd
dorislee0309 <dorislee@berkeley.edu>
2016-11-03 11:04:10 -0500

    debug set

--- web-app/run.py
+++ web-app/run.py
@@ -1,3 +1,6 @@
 from app import app
+import logging
 app.run(debug=True)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']

1c62c573d20cae56e6e9856e2016a5baeb5e0264
dorislee0309 <dorislee@berkeley.edu>
2016-11-19 22:58:07 -0600

    changed randkey drawing for obj and objectlocation into python rather than in JS page.html

--- web-app/app/views.py
+++ web-app/app/views.py
@@ -38,18 +38,25 @@ def segment(img):
 	objects = models.Object.query.filter_by(image_id=image_id).order_by(models.Object.name).all()
 	#Read object locations for these objects
 	object_locations = models.ObjectLocation.query.filter((models.ObjectLocation.object_id.in_([x.id for x in objects]))).all()
-
-	#Make this data easy to use
-	objects = {x.id:x.name for x in objects}
-	object_locations = {x.object_id:(x.x_loc,x.y_loc) for x in object_locations} #ASSUMES THAT EACH OBJECT IS MARKED BY EXACTLY 1 WORKER
-
+	if len(objects)!=0:
+		#Make this data easy to use
+		objects = {x.id:x.name for x in objects}
+		object_locations = {x.object_id:(x.x_loc,x.y_loc) for x in object_locations} #ASSUMES THAT EACH OBJECT IS MARKED BY EXACTLY 1 WORKER
+		print objects
+		print object_locations
+		randkey=objects.keys()[randint(0,len(objects)-1)]
+		obj = objects[randkey]
+		objloc = object_locations[randkey]
+	else:
+		obj=""
+		objloc=""
 	#The following code segment can be used to check if the turker has accepted the task yet
 	if request.args.get("assignmentId") == "ASSIGNMENT_ID_NOT_AVAILABLE":
 		#Our worker hasn't accepted the HIT (task) yet
-		resp = make_response(render_template('page.html',name=render_data,filename=filename,ht=384,wd=512,accepted=False,objects=objects,locs=object_locations,img=img))
+		resp = make_response(render_template('page.html',name=render_data,filename=filename,ht=384,wd=512,accepted=False,object=obj,loc=objloc,img=img))
 	else:
 		#Our worker accepted the task
-		resp = make_response(render_template('page.html',name=render_data,filename=filename,ht=384,wd=512,accepted=True,objects=objects,locs=object_locations,img=img))
+		resp = make_response(render_template('page.html',name=render_data,filename=filename,ht=384,wd=512,accepted=True,object=obj,loc=objloc,img=img))
 	resp.headers['x-frame-options'] = 'this_can_be_anything'
 	return resp
 

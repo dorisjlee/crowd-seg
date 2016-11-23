554c7bccdd7624394c0f942f67f101079c844d8a
dorislee0309 <dorislee@berkeley.edu>
2016-11-22 23:07:06 -0600

    unable to check for unique worker obj submissions for integrity constraints

--- analysis/post_to_mturk.py
+++ analysis/post_to_mturk.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
 import time
+import glob
 from boto.mturk.connection import MTurkConnection
 from boto.mturk.question import ExternalQuestion
 from boto.mturk.qualification import Qualifications, PercentAssignmentsApprovedRequirement, NumberHitsApprovedRequirement #, Requirement
@@ -7,7 +8,6 @@ from boto.mturk.price import Price
 from secret import SECRET_KEY,ACCESS_KEY
 import os
 from analysis_toolbox import *
-
 DEV_ENVIROMENT_BOOLEAN = True
 #This allows us to specify whether we are pushing to the sandbox or live site.
 if DEV_ENVIROMENT_BOOLEAN:
@@ -16,7 +16,7 @@ if DEV_ENVIROMENT_BOOLEAN:
 else:
     AMAZON_HOST = 'mechanicalturk.amazonaws.com'
     MastersQualID = '2NDP2L92HECWY8NS8H3CK0CP5L9GHO'
-os.chdir("app")
+
 #Start Configuration Variables
 AWS_ACCESS_KEY_ID = ACCESS_KEY
 AWS_SECRET_ACCESS_KEY = SECRET_KEY
@@ -42,15 +42,14 @@ qualifications.add(NumberHitsApprovedRequirement(comparator="GreaterThan", integ
 img_info,object_tbl,bb_info,hit_info  = load_info()
 img_obj_tbl = object_tbl.merge(img_info,how="inner",left_on="image_id",right_on="id")
 
+os.chdir("../web-app/app")
 #This url will be the url of your application, with appropriate GET parameters
 with open('ActiveHITs','a') as f:
 	f.write('New batch created on : '+time.ctime())
 	for fname in glob("static/COCO_*.png")[:2]:
 		img_name = fname.split('/')[-1].split('.')[0]
+		print img_name
 		if HIT_TYPE == "IDENTIFY":
-			numObj = len(img_obj_tbl[img_obj_tbl.filename==fname])
-			print numObj, "obj in image :",fname
-			print "max_assignments:", 30*numObj
 			url = "https://crowd-segment.herokuapp.com/identify/{}".format(img_name)
 			questionform = ExternalQuestion(url, frame_height)
 			create_hit_result = connection.create_hit(
@@ -58,7 +57,7 @@ with open('ActiveHITs','a') as f:
 				description="We'll give you an image, and you have to identify objects inside the image. There is 1 image per HIT (and we'll only load it once). Our interface supports keyboard input for speed!",
 				keywords=["identification", "perception", "image", "fast"],
 				duration = 1800,
-				max_assignments=30*numObj,
+				max_assignments=20,
 				question=questionform,
 				reward=Price(amount=0.05),
 				lifetime=43200)#,
@@ -67,18 +66,22 @@ with open('ActiveHITs','a') as f:
 			f.write(hit_id + "\n")
 			print "Created HIT: ",hit_id
 		elif HIT_TYPE == "SEGMENT":
+			numObj = len(img_obj_tbl[img_obj_tbl.filename==img_name])
+			print numObj, "obj in image :",img_name
+			print "max_assignments:", 30*numObj
 			url = "https://crowd-segment.herokuapp.com/{}".format(img_name)
 			questionform = ExternalQuestion(url, frame_height)
-			create_hit_result = connection.create_hit(
-				title="Segment the object on an image",
-				description="We'll give you an image with a pointer to an object. You have to draw a bounding region around the boundary of the object in the image. There is 1 object per HIT. Our interface supports keyboard input for speed!",
-				keywords=["segmentation", "perception", "image", "fast"],
-				duration = 1800,
-				max_assignments=30,
-				question=questionform,
-				reward=Price(amount=0.05),
-				lifetime=43200)#,
-				#qualifications=qualifications)
-			hit_id = str(create_hit_result[0].HITId)
-			f.write(hit_id + "\n")
-			print "Created HIT: ",hit_id
+			for _i in range(20*numObj):
+				create_hit_result = connection.create_hit(
+					title="Segment the object on an image",
+					description="We'll give you an image with a pointer to an object. You have to draw a bounding region around the boundary of the object in the image. There is 1 object per HIT. Our interface supports keyboard input for speed!",
+					keywords=["segmentation", "perception", "image", "fast"],
+					duration = 1800,
+					max_assignments=1,
+					question=questionform,
+					reward=Price(amount=0.05),
+					lifetime=43200)#,
+					#qualifications=qualifications)
+				hit_id = str(create_hit_result[0].HITId)
+				f.write(hit_id + "\n")
+				print "Created HIT: ",hit_id

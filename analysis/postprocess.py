a11e2b90608c5d94967b01af358aad882d1e6cbc
dorislee0309 <dorislee@berkeley.edu>
2016-12-01 21:06:32 -0600

    post processing experiment results

--- analysis/postprocess.py
+++ analysis/postprocess.py
@@ -35,25 +35,26 @@ else:
 
 hit_info['status'] = pd.Series(['undetermined' for _i in range(len(hit_info))], index=hit_info.index)
 object_tbl['approvedBB_count'] = pd.Series(np.zeros(len(object_tbl)), index=object_tbl.index)
-print object_tbl
+print object_tbl #must have
 for hit in all_hits:
     assignments = connection.get_assignments(hit.HITId)
-    print assignments
     for assignment in assignments:
         task = hit_info[hit_info.hit_id==assignment.HITId]
-        print assignment
-        print task
         if len(task)>0:
             actions = ast.literal_eval(task.actions.get_values()[0])
             numClicks = actions.count("addClick")
             try:
                 if numClicks >=3:
-                    print 'approving ', assignment.AssignmentId
                     hit_info = hit_info.set_value(task.index[0],'status',"approved")
                     #Add additional counts to object table
-                    new_approved_count= object_tbl._iloc[int(task.object_id)].approvedBB_count+1
-                    object_tbl = object_tbl.set_value(task.object_id,'approvedBB_count', new_approved_count)
+                    # new_approved_count= object_tbl._iloc[int(task.object_id)].approvedBB_count+1
+                    try:
+                        new_approved_count=object_tbl._iloc[task.object_id].approvedBB_count.get_values()[0]+1
+                        object_tbl = object_tbl.set_value(task.object_id,'approvedBB_count', new_approved_count)
+                    except(IndexError):
+                        print "something wrong with updating indexing "
                     connection.approve_assignment(assignment.AssignmentId)
+                    print 'approved ', assignment.AssignmentId
                 else:
                     print 'Putting into pending reject list :', assignment.AssignmentId
                     bb = bb_info[(bb_info.worker_id==int(task.worker_id)) & (bb_info.object_id==int(task.object_id))]
@@ -63,7 +64,7 @@ for hit in all_hits:
                     #connection.reject_assignment(assignment.AssignmentId)
             except MTurkRequestError:
                 #Problably already approved or rejected this assignment previously
-               print "Problem with rejecting/approving : ", assignment.AssignmentId
+               print "Previously rejected or approved : ", assignment.AssignmentId
                error_log.write(assignment.AssignmentId+'\n')
 error_log.close()
 object_tbl.to_csv('../../data/approved_object_tbl.csv')

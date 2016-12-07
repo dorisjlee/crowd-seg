9188787102de8741a2dd299fb5af6f125e30b35a
dorislee0309 <dorislee@berkeley.edu>
2016-12-07 11:29:09 -0600

    COCO Majority Vote computed

--- analysis/qualityBaseline.py
+++ analysis/qualityBaseline.py
@@ -14,14 +14,14 @@ def majority_vote(obj_x_locs,obj_y_locs,width,height):
     img = Image.new('L', (width, height), 0)
     for x_locs, y_locs in zip(obj_x_locs,obj_y_locs):
         ImageDraw.Draw(img).polygon(zip(x_locs,y_locs), outline=1, fill=1)
-        mask = numpy.array(img)==1
+        mask = np.array(img)==1
     #     plt.imshow(mask)
         mega_mask+=mask
     #     plt.plot(x_locs,y_locs,'-',color="#f442df",linewidth=5)
     #     plt.fill_between(x_locs,y_locs,  color="none",facecolor='#f442df', alpha=0.4)
     # Show Majority Vote area
-    plt.imshow(mega_mask)
-    plt.colorbar()
+    # plt.imshow(mega_mask)
+    # plt.colorbar()
     # Compute Jaccard Simmilarity 
     intersection = len(np.where(mega_mask == mega_mask.max())[0])
     union  =len(np.where(mega_mask !=0)[0])

8d6850f9bb3a45d37520755ba6f506427f9c29f5
dorislee0309 <dorislee@berkeley.edu>
2016-12-19 08:02:20 +0800

    tried fitting rvs ks test

--- analysis/secret.py
+++ analysis/secret.py
@@ -1,6 +1,6 @@
 ACCESS_KEY = 'AKIAIZQ3EQ4YZMDKA4RA'
 SECRET_KEY = '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'
-DEV_ENVIROMENT_BOOLEAN = True
+DEV_ENVIROMENT_BOOLEAN = False
 #This allows us to specify whether we are pushing to the sandbox or live site.
 if DEV_ENVIROMENT_BOOLEAN:
     AMAZON_HOST = 'mechanicalturk.sandbox.amazonaws.com'

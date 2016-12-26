bb5eb068bdeb283ff31832ee674649fcdf49eccc
dorislee0309 <dorislee@berkeley.edu>
2016-12-26 23:02:34 +0800

    fitted Ji distributions gaussian, working on linear regression for SD v.s. BB complexity

--- analysis/analysis_toolbox.py
+++ analysis/analysis_toolbox.py
@@ -5,6 +5,7 @@ from glob import glob
 from os.path import expanduser
 import pandas as pd 
 from PIL import Image
+import numpy as np
 def save_db_as_csv(db="crowd-segment",connect=True,postgres=True):
 	'''
 	Create CSV file of each table from app.db
@@ -80,3 +81,68 @@ def visualize_my_ground_truth_bb():
                     x_locs,y_locs =  process_raw_locs([ground_truth_match["x_locs"].iloc[0],ground_truth_match["y_locs"].iloc[0]])
                     plt.plot(x_locs,y_locs,'-',color='#f442df',linewidth=0.5)
                     plt.fill_between(x_locs,y_locs,color='none',facecolor='#f442df', alpha=0.01)
+############################################################
+############################################################
+############		DATA FITTING MODULES			########
+############################################################
+############################################################
+from scipy import stats
+def pcheck(p,null_hyp):
+    '''
+    if p>0.05 then reject null hypothesis
+    '''
+    if p>0.05:
+        return  null_hyp
+    else:
+        return "NOT "+null_hyp
+def one_way_kolmogorov_smirnov(data,name,distr_name):
+	'''
+	See if data come from the reference distribution
+	'''
+	result = stats.kstest(data,distr_name)
+	print "{0} : D = {1} ; p ={2} ---> {3}".format(name,np.around(result[0],2),np.around(result[1],2),pcheck(result[1],"from {} distribution".format(distr_name)))
+
+def basic_stat(data1,name):
+	print "{0}: mu = {1}; std = {2}".format(name,np.around(mean(data1),3),np.around(std(data1),3))
+	return [name,np.around(mean(data1),3),np.around(std(data1),3)]
+
+def basic_stats(data1,data2,mode="double"):
+	print "Precision: mu = {0}; std = {1}".format(np.around(mean(data1),3),np.around(std(data1),3))
+	if mode=="double": print "Recall: mu = {0}; std = {1}".format(np.around(mean(data2),3),np.around(std(data2),3))
+
+def kolmogorov_smirnov(data1,data2,name):
+	'''
+	Two-way KS test . See if data come from the same distribution
+	'''
+	result = stats.ks_2samp(data1,data2)
+	if result[1]>0.05: print "{0} : D = {1} ; p ={2} ---> {3}".format(name,np.around(result[0],2),np.around(result[1],2),pcheck(result[1],"from same distribution"))
+	return result
+def plot_fitted_worker_histo(fcn):
+    metrics_lst = ['Precision [COCO]','Recall [COCO]','Jaccard [COCO]',\
+                   'Precision [Self]','Recall [Self]','Jaccard [Self]']
+    NUM_COL = 3
+    NUM_ROW = 2
+    NUM_PLOTS = NUM_COL*NUM_ROW
+
+    fig, axs = plt.subplots(NUM_ROW,NUM_COL, figsize=(NUM_ROW*3,NUM_COL*1.5), sharex='col')
+    stitle = fig.suptitle("Worker Metric Distribution [{}] ".format(fcn.name),fontsize=16,y=1.05)
+
+    axs = axs.ravel()
+    table_data = []
+    for i,metric in zip(range(len(metrics_lst)),metrics_lst):
+        metric_value = np.array(bb_info[metric][bb_info[metric]>FILTER_CRITERION][bb_info[metric]<=1]) 
+        ax = axs[i]
+        ax.set_title(metric)
+        #ax.hist(metric_value,bins=30)
+        ax.set_xlim(FILTER_CRITERION,1.03)
+
+
+        metric_value = np.array(bb_info[metric][bb_info[metric]>0][bb_info[metric]<=1]) 
+        params = fcn.fit(metric_value)
+    #     histo,bin_edges = np.histogram(metric_value, 50, normed=1)
+    #     bins = ((bin_edges+np.roll(bin_edges,-1))/2)[:-1]
+        n, bins, patches = ax.hist(metric_value, 40, normed=1, facecolor='blue', alpha=0.75)
+        y = fcn.pdf(bins, *params)
+        l = ax.plot(bins, y, 'r--', linewidth=2) 
+    fig.tight_layout()
+    fig.savefig('{}_fitted_metric_histogram.pdf'.format(fcn.name), bbox_inches='tight',bbox_extra_artists=[stitle])

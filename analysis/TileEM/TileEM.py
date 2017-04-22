119c995fd492b24867334066ff32343a6b5284b5
dorislee0309 <dorislee@berkeley.edu>
2017-04-22 17:57:06 -0500

    tileEM experiments

--- analysis/TileEM/TileEM.py
+++ analysis/TileEM/TileEM.py
@@ -9,7 +9,7 @@ def estimate_Qj(T,tiles,indMat,workers,Qjfunc,A_percentile):
     for wid,j in zip(workers,range(len(workers))):
         Qj.append(Qjfunc(tiles,indMat,T,j,A_percentile))
     return Qj
-def computeT(objid,T,tiles,indMat,workers,Tprime_lst,Qj,pTprimefunc,A_percentile,PLOT_LIKELIHOOD=False):
+def computeT(objid,T,tiles,indMat,workers,Tprime_lst,Qj,pTprimefunc,A_percentile,DEBUG=False,PLOT_LIKELIHOOD=False):
     # Loop through Tprime_lst find the argmax T' s.t pTprime is max given fixed Qj
     pTprime_lst =[]
     for Tprime_idx in Tprime_lst:
@@ -18,13 +18,9 @@ def computeT(objid,T,tiles,indMat,workers,Tprime_lst,Qj,pTprimefunc,A_percentile
     Tidx= np.argmax(pTprime_lst)
     max_likelihood =pTprime_lst[Tidx]
     if PLOT_LIKELIHOOD:
-        tidx_score = np.zeros(len(tiles))
-        pTprime_lst= [123,412,124]
-        Tprime_idx=[1,3,18]
-        for i,tidx in enumerate(Tprime_idx):
-            tidx_score[Tprime_idx[i]]=pTprime_lst[i]
-    	visualizeTilesScore(tiles,tidx_score,INT_Z=False,colorful=True)
-    return Tprime_lst[Tidx],join_tiles(Tprime_lst[Tidx],tiles)[0],max_likelihood
+    	visualizeTilesScore(tiles,dict(zip(range(len(Tprime_lst)),pTprime_lst)),INT_Z=False,colorful=True)
+    if DEBUG: print "Likelihood:{0} ; T={1}".format(max_likelihood,Tprime_lst[Tidx])
+    return pTprime_lst,Tprime_lst[Tidx],join_tiles(Tprime_lst[Tidx],tiles)[0],max_likelihood
 ###################################################################################################################################################
 ###################################################################################################################################################
 ############################################################### Getting Ground Truth  #############################################################
@@ -36,8 +32,8 @@ def initT(tiles,indMat):
     # where we combine the area and vote in a 1:2 ratio
     area = np.array(indMat[-1])
     votes =indMat[:-1].sum(axis=0)
-    norm_area_vote = area/max(area)+2*votes/max(votes)
-    tidx = np.argsort(norm_area_vote)[::-1][:5]
+    norm_area_vote = area/max(area)+5*votes/max(votes)
+    tidx = np.argsort(norm_area_vote)[::-1][:2]
     return join_tiles(tidx,tiles)[0]
 
 def ground_truth_T(object_id):
@@ -51,8 +47,9 @@ def ground_truth_T(object_id):
 ############################################################### T prime search strategy  ##########################################################
 ###################################################################################################################################################
 ###################################################################################################################################################
-def Tprime_snowball_area(objid,indMat,fixedtopk=5, topk = 40,NTprimes=300):
+def Tprime_snowball_area(objid,indMat,fixedtopk=3, topk = 40,NTprimes=300):
     # Select weighted area-vote score top tiles
+    np.random.seed(111)
     area = np.array(indMat[-1])
     votes =indMat[:-1].sum(axis=0)
     norm_area_vote = area/max(area)+2*votes/max(votes)
@@ -69,7 +66,7 @@ def Tprime_snowball_area(objid,indMat,fixedtopk=5, topk = 40,NTprimes=300):
         rand_subset.append(tidxInCombo)
     return rand_subset
 
-def runTileEM(objid,Tprimefunc,pTprimefunc,Qjfunc,A_percentile,Niter,DEBUG=False):
+def runTileEM(objid,Tprimefunc,pTprimefunc,Qjfunc,A_percentile,Niter,NTprimes=100,DEBUG=False,PLOT_LIKELIHOOD=False):
     '''
     Tfunc : how to get ground truth 
     Tprimefunc : how to pick T'
@@ -80,10 +77,10 @@ def runTileEM(objid,Tprimefunc,pTprimefunc,Qjfunc,A_percentile,Niter,DEBUG=False
     tiles = pkl.load(open(DATA_DIR+"/vtiles{}.pkl".format(objid)))
     indMat = pkl.load(open(DATA_DIR+"/indMat{}.pkl".format(objid)))
     workers = pkl.load(open(DATA_DIR+"/worker{}.pkl".format(objid)))
-    likelihood_lst=[]
     T_lst = []
+    pTprime_lst=[]
     if DEBUG: print "Coming up with T' combinations to search through" 
-    Tprime_lst = Tprimefunc(objid,indMat,fixedtopk=5, topk = 40,NTprimes=500)
+    Tprime_lst = Tprimefunc(objid,indMat,fixedtopk=3, topk = 40,NTprimes=NTprimes)
     for _i in tqdm(range(Niter)):
         if _i ==0:
             T=initT(tiles,indMat)
@@ -91,11 +88,15 @@ def runTileEM(objid,Tprimefunc,pTprimefunc,Qjfunc,A_percentile,Niter,DEBUG=False
         Qjhat = estimate_Qj(T,tiles,indMat,workers,Qjfunc,A_percentile)
         ####potentially adaptive way of getting a new set of T', currently a fixed set of T' #####
         if DEBUG: print "Mstep: Picking the max-likelihood T' " 
-        Tidx,T, max_likelihood= computeT(objid,T,tiles,indMat,workers,Tprime_lst,Qjhat,pTprimefunc,A_percentile)
-        likelihood_lst.append(max_likelihood)
+        pTprimes,Tidx,T, max_likelihood= computeT(objid,T,tiles,indMat,workers,Tprime_lst,Qjhat,pTprimefunc,A_percentile,PLOT_LIKELIHOOD=PLOT_LIKELIHOOD,DEBUG=DEBUG)
+        pTprime_lst.append(pTprimes)
         T_lst.append(Tidx)
-    return T_lst,likelihood_lst
+    return pTprime_lst,T_lst
 
 if __name__ =="__main__":
-	DATA_DIR="final_all_tiles"
-	T_lst,likelihood_lst = runTileEM(2,Tprime_snowball_area,pTprimeGTLSA,QjGTLSA,99,10)
+	DATA_DIR="sampletopworst5"
+	for objid in [14,3,47]:
+		pTprime_lst,T_lst = runTileEM(objid,Tprime_snowball_area,pTprimeGTLSA,QjGTLSA,A_percentile=99,\
+	                                             Niter=5,NTprimes=2000,PLOT_LIKELIHOOD=True,DEBUG=True)
+		pkl.dump(pTprime_lst,open("pTprime_exp#{0}_obj{1}.pkl".format(exp_num,objid),'w'))
+		pkl.dump(T_lst,open("pTprime_exp#{}_obj{1}.pkl".format(exp_num,objid),'w'))
